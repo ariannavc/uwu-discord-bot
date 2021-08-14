826ea7a342d07167f22fe83b94290a720a9987d9
Arianna <48294225+ariannavc@users.noreply.github.com>
2021-08-14 15:04:34 +0000

    Added simple greeting mechanism

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ TOKEN = os.environ['DISCORD_BOT_SECRET']
 
 client = discord.Client()
 
+greetings = ['hello','hi','hii','hiii','hewwo','greetings']
 
 @client.event
 async def on_ready():
@@ -21,9 +22,16 @@ async def on_message(message):
   if message.author == client.user:
     return
   if 'uwu' in message.content.lower():
+    if any(greeting in message.content.lower() for greeting in greetings):
+      await message.channel.send('hewwo! ^.^')
     await message.channel.send('uwu')
   if 'owo' in message.content.lower():
+    if any(greeting in message.content.lower() for greeting in greetings):
+      await message.channel.send('hewwo! ^.^')
     await message.channel.send('owo')
+  if any(greeting == message.content.lower() for greeting in greetings):
+    await message.channel.send('hewwo! ^.^')
+        
 
 
 client.run(TOKEN)
