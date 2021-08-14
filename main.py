7cda55af457fa335b7c4659d7f83301c506df189
Arianna <48294225+ariannavc@users.noreply.github.com>
2021-08-14 13:54:24 +0000

    Gave bot basic functionality.
    It will now reply "uwu" to any message containing "uwu" and will reply "owo" to any message containing "owo.

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
-import discord
 
+
+import discord
+import os
+
+
+TOKEN = os.environ['DISCORD_BOT_SECRET']
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+  print('i did it! ^.^')
+  print(client.user)
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+  if 'uwu' in message.content:
+    await message.channel.send('uwu')
+  if 'owo' in message.content:
+    await message.channel.send('owo')
+
+
+
+client.run(TOKEN)
